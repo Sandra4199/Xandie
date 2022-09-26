5a83833a25abd49b5b7fe012d446642633346bfc
Sandra4199 <113185080+Sandra4199@users.noreply.github.com>
2022-09-26 10:01:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+let x=100;
+let y=200;
+
+let z=x+y; 
+let X=15;
+let myVar=25;
+if (X===myvar){
+    
+    console.log("they are equal")
+}else{
+console.log("they are not equal")
+}
